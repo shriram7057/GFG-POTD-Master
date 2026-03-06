1a85b3d7f23cc644bd299a4a5c0ed908176ace99
Mr.Shree <172600005+shriram7057@users.noreply.github.com>
2026-03-07 00:59:13 +0530

    Add files via upload

---
+++ GFG-POTD-Solutions/Dice throw.py
@@ -0,0 +1,12 @@
+class Solution:
+    def noOfWays(self, m,n,x):
+        # code here
+        dp = [[0]*(x+1)for _ in range(n+1)]
+        dp[0][0]= 1
+        
+        for i in range(1,n+1):
+            for j in range(1,x+1):
+                for face in range(1,m+1):
+                    if j - face >= 0:
+                        dp[i][j]+= dp[i-1][j-face]
+        return dp[n][x]
